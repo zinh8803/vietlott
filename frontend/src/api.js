041925c10cf7b99909415fd763247efa8e7f9e1f
vinhnq <ngoquocvinh2003@gmail.com>
2026-07-09 16:00:07 +0700

    add env fe

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
   timeout: 240000,
 });
 
